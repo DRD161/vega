5c1d6e17fff48f02ce0f452c4582f959629043e7
DRD161 <dylan@dylandavenport.com>
2025-02-09 11:25:42 -0500

    Update pages.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,19 +2,20 @@ import { fetchChartData } from "@/lib/utils";
 import ErrorToast from "@/components/ErrorToast";
 
 import DoughnutChart from "@/components/DoughnutChart";
+import TableComponent from "@/components/TableComponent";
 
 const Dashboard = async () => {
   try {
-    // const { assetsData, portfolioData, pricesData } = await fetchChartData();
-
-    const { portfolioData } = await fetchChartData();
-
+    const portfolioData = await fetchChartData(
+      "http://localhost:3000/api/portfolio",
+    );
     return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="grid auto-rows-auto items-center justify-items-center min-h-screen">
+        <section>
           <DoughnutChart data={portfolioData} />
-        </main>
-      </div>
+          <TableComponent data={portfolioData} />
+        </section>
+      </main>
     );
   } catch (error: unknown) {
     console.error("Error fetching data:", error);
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import LoginPage from "@/components/LoginPage";
+import LoginForm from "@/components/LoginForm";
 
 const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen">
-      <main className="flex flex-col gap-8 row-start-2">
-        <LoginPage />
-      </main>
-    </div>
+    <main className="grid auto-rows-auto items-center justify-items-center min-h-screen">
+      <LoginForm />
+    </main>
   );
 };
 
